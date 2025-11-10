6378d65714733b2f43cffc5a00769ceb4efb74d9
Dimbog <Bogateev-97@mail.ru>
2025-11-10 19:34:34 +0300

    Update uni_password_test.py

--- Day_1/unittest_examples/uni_password_test.py
+++ Day_1/unittest_examples/uni_password_test.py
@@ -5,16 +5,22 @@ import Day_1.Practice.strong_password as strong_password
 
 class TestPassword(unittest.TestCase):
     def test_check_password(self):
-        self.assertTrue(all(strong_password.check_password("123456")))
+        check, msg = strong_password.check_password("123456")
+        self.assertFalse(check)
+        self.assertRegex(msg, 'не')
 
     def test_check_password_one(self):
-        self.assertTrue(all(strong_password.check_password("123456478!")))
+        check, msg = strong_password.check_password("123456478!")
+        self.assertTrue(check)
 
     def test_check_password_two(self):
-        self.assertTrue(all(strong_password.check_password("saucacAusacu8")))
+        check, msg = strong_password.check_password("saucacAusacu8")
+        self.assertTrue(check)
 
     def test_check_password_three(self):
-        self.assertTrue(all(strong_password.check_password("o58anuahaunH!")))
+        check, msg = strong_password.check_password("o58anuahaunH!")
+        self.assertTrue(check)
+        self.assertRegex(msg, 'Пароль введен корректно')
 
 
 if __name__ == "__main__":
