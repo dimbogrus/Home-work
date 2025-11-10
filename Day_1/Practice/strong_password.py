cf95a45ece848b6c61a71c2667e5d5e18d52f663
Dimbog <Bogateev-97@mail.ru>
2025-11-10 17:18:08 +0300

    Update strong_password.py

--- Day_1/Practice/strong_password.py
+++ Day_1/Practice/strong_password.py
@@ -45,56 +45,60 @@ st.ascii_uppercase
 special = "_!@#$%^&"
 
 
-def check_password(password: str) -> tuple:
-    dig = False
+def check_password(password: str) -> tuple[bool, str]:
+    dig = low = up=  =False
     dig_text = "Не использовались цифры регистр"
-    low = False
     low_text = "Не использовался нижний регистр"
-    up = False
     up_text = "Не использовался верхний регистр"
-    spec = False
     spec_text = 'Не использовался спецсимвол "_!@#$%^&"'
 
-    for i in password:
-        if i in st.digits:
-            dig = True
-            dig_text = "Цифры использовались"
-        if i in st.ascii_lowercase:
-            low = True
-            low_text = "Использовался нижний регистр"
-        if i in st.ascii_uppercase:
-            up = True
-            up_text = "Использовался верхний регистр"
-        if i in special:
-            spec = True
-            spec_text = 'Использовался спецсимвол "_!@#$%^&"'
-    check = (dig, dig_text, low, low_text, up, up_text, spec, spec_text)
+    if 8 <= len(password) <= 15:
+        for i in password:
+            if i in st.digits:
+                dig = True
+                dig_text = "Цифры использовались"
+            if i in st.ascii_lowercase:
+                low = True
+                low_text = "Использовался нижний регистр"
+            if i in st.ascii_uppercase:
+                up = True
+                up_text = "Использовался верхний регистр"
+            if i in special:
+                spec = True
+                spec_text = 'Использовался спецсимвол "_!@#$%^&"'
+        check_all = (
+            dig,
+            dig_text,
+            low,
+            low_text,
+            up,
+            up_text,
+            spec,
+            spec_text,
+        )
+
+        if all(check_all):
+            check = (True, "Пароль введен корректно")
+        print("Неудача")
+        print(check_all)
+        check = (False, "Условие не соблюдены, смотри лог")
+    else:
+        print("Неудача")
+        check = (False, "Длина пароля не соответсвуют условию")
+
     return check
 
 
-def len_password():
-    schet = 0
-    try_password = True
-    while try_password:
-        if schet < 4:
-            password = input("Введите пароль: ")
-            schet += 1
-            print(f"Попытка ввода пароля: {schet}")
-            if 8 <= len(password) <= 15:
-                result = check_password(password)
-                if False in result:
-                    print("Неудача")
-                    print(result)
-                else:
-                    print("Пароль введен корректно")
-                    try_password = False
-            else:
-                print("Неудача")
-                print("Длина пароля не соответсвуют условию")
-        else:
-            try_password = False
+def input_password():
+    for schet in range(0, 5):
+        password = input("Введите пароль: ")
+        print(f"Попытка ввода пароля: {schet+1}")
+        check, msg = check_password(password)
+        print(msg)
         print("-" * 20)
+        if check:
+            break
 
 
 if __name__ == "__main__":
-    len_password()
+    input_password()
