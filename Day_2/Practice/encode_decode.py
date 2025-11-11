b683469f6e1c6a234d43bc8a73257e9baeb3cf48
Dimbog <Bogateev-97@mail.ru>
2025-11-11 17:07:03 +0300

    day2

--- Day_2/Practice/encode_decode.py
+++ Day_2/Practice/encode_decode.py
@@ -13,3 +13,51 @@ In/Out: '070411111426152419071413' <-> Out/In: 'hello python'
 1.4 Написать тесты для отработки корректных и некорректных данных.
 
 """
+
+import string as st
+
+st.ascii_lowercase
+
+
+def sozdanie_dict() -> dict:
+    translate_dict = {}
+    for a in range(00, 26):
+        if a < 10:
+            translate_dict[f"0{a}"] = st.ascii_lowercase[a]
+        else:
+            translate_dict[f"{a}"] = st.ascii_lowercase[a]
+    translate_dict[f"26"] = " "
+    return translate_dict
+
+
+def encode(sms: str) -> str:
+    if not isinstance(sms, str):
+        raise TypeError(f"Не правильный тип {type(sms)}")
+    if len(sms) % 2 != 0:
+        raise ValueError("Длина сообщения должна быть делима на 2 без остатка")
+    shifr = sozdanie_dict()
+    result = ""
+    step_slice = 0
+    for slice in range(2, len(sms) + 1, 2):
+        number = sms[step_slice:slice]
+        step_slice = slice
+        result += shifr[number]
+    return result
+
+
+print(encode("070411111426152419071413"))
+
+
+def decoder(sms: str) -> str:
+    if not isinstance(sms, str):
+        raise TypeError(f"Не правильный тип {type(sms)}")
+    shifr = sozdanie_dict()
+    result = ""
+    for slice in sms:
+        for k, v in shifr.items():
+            if v == slice:
+                result += k
+    return result
+
+
+print(decoder("hello python"))
---
+++ Day_2/Practice/encode_decode_test.py
@@ -0,0 +1,17 @@
+import unittest
+
+import Day_2.Practice.encode_decode as ed
+
+
+class TestEncodeDecode(unittest.TestCase):
+    """Test encode"""
+    def test_encode(self):
+        self.assertEqual(ed.encode('070411111426152419071413'), 'hello python')
+
+    """Test dencode"""
+    def test_dencoder(self):
+        self.assertEqual(ed.decoder('hello python'), '070411111426152419071413')
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
