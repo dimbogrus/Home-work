329ecd1fd8a5a1242038eee064870e0f163cd8d0
Dimbog <Bogateev-97@mail.ru>
2025-11-11 17:11:13 +0300

    day2

--- Day_2/Practice/encode_decode_test.py
+++ Day_2/Practice/encode_decode_test.py
@@ -1,17 +1,19 @@
 import unittest
 
-import Day_2.Practice.encode_decode as ed
+import encode_decode as ed
 
 
 class TestEncodeDecode(unittest.TestCase):
     """Test encode"""
     def test_encode(self):
         self.assertEqual(ed.encode('070411111426152419071413'), 'hello python')
+        self.assertRaises(TypeError, ed.encode, 110411111426152419071413)
+        self.assertRaises(ValueError, ed.encode, 'hello python')
 
     """Test dencode"""
     def test_dencoder(self):
         self.assertEqual(ed.decoder('hello python'), '070411111426152419071413')
-
+        self.assertRaises(TypeError, ed.decoder, 110411111426152419071413)
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
